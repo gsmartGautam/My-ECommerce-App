cd624094360b81853e390a6647fed5fa807411fb
Gautam Rana <gsmartgautam@gmail.com>
2022-07-24 01:54:04 +0530

    added project

---
+++ client/src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.pinimg.com/originals/b8/ac/1b/b8ac1b662bf545ee865c67edf71c8926.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+      },
+      {
+        id: 2,
+        img: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/14709966/2021/7/10/d2407657-1f04-4d13-9f52-9e134050489b1625905793495-Nayo-Women-Red-Ethnic-Motifs-Printed-Empire-Pure-Cotton-Kurt-1.jpg",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+      },
+      {
+        id: 3,
+        img: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/2525392/2022/3/31/686778a7-19c3-4021-94ff-5d4f460ecd031648716950163RAREBlueFloralPrintedMaxiDress1.jpg",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+      },
+];
+
+export const categories = [
+    {
+      id: 1,
+       img: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/7256771/2018/8/31/a92f8669-e902-400d-bf29-a060d30118581535699793413-NA-6081535699793230-1.jpg",
+       title: "SHIRT STYLE!",
+       cat:"women"
+    },
+    {
+      id: 2,
+      img: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/productimage/2020/9/22/7e999732-a4ad-4464-9d57-5e0bd60a270c1600728478352-1.jpg",
+       title: "LOUNGEWEAR LOVE",
+       cat:"coats"
+    },
+    {
+      id: 3,
+      img: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/v1/image/style/properties/807935/La-Zoire-Women-Tops_1_ebbbac2b1b154db3f368aa08e9c86d5f.jpg",
+       title: "LIGHT JACKETS",
+       cat:"jeans"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+       img: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/7383105/2019/12/6/d4163c0d-0a30-49c3-a252-7f8c0552ca6c1575621149446-her-by-invictus-Women-Blue-Self-Design-Sweater-Dress-9561575-1.jpg",
+    },
+    {
+      id:2,
+      img: "https://cdn2.stylecraze.com/wp-content/uploads/2018/01/3.-Myntra.jpg",
+    },
+    {
+      id:3,
+      img: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/16491102/2021/12/13/f9b352a1-23b0-4819-bc5b-4cf4c6b514f01639372880401PinkChickNavyBlueMulticolouredLayeredSatinDress1.jpg",
+    },
+    {
+      id:4,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZFGDUQp-tnOmb9nl02YQ8SKdy-2uixoRX8A&usqp=CAU",
+    },
+    {
+      id:5,
+      img: "https://lzd-img-global.slatic.net/g/p/b5dc8e012464522ea6049f031253f467.jpg_720x720q80.jpg_.webp",
+    },
+    {
+      id:6,
+      img: "https://i.pinimg.com/564x/2c/27/b8/2c27b844b486474905c4ac6ba6619adc.jpg",
+    },
+    {
+      id:7,
+      img: "https://5.imimg.com/data5/SELLER/Default/2021/9/MF/TP/VC/108340265/m-rts-11-fashionzilla-original-imafz822q8p9uejf-500x500.jpeg",
+    },
+    {
+      id:8,
+      img: "https://lh3.googleusercontent.com/RCdW-4gkACN4DkMpxJ3MhIaKC7oNwM83uQnie25CHcKLcHC7hmfabaUH7k4ZvgMrbxm3okPHpu6_QpnLlrIhMGVFMZ_kuL6mWEdYd-s=w1000?s=300",
+    },
+  ]
